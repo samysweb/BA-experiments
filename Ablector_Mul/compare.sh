fd24a29d2522fc468f2507d879ff328675045171
Samuel Teuber <samuel@samweb.org>
2019-06-23 19:53:20 +0200

    Added analysis for single stage abstraction

--- Ablector_Mul/compare.sh
+++ Ablector_Mul/compare.sh
@@ -1,14 +1,31 @@
-mkdir -p results
-cd results
-python ../../logParsing/buildCsv.py 2 ../../Logs/Ablector_Mul/segment_aa/2019-06-17T16:50+02:00 | sort > ablector.csv
-python ../../logParsing/buildCsv.py 2 ../../Logs/Boolector_SoA/segment_aa/2019-06-17T16:49+02:00 | sort > boolector.csv
-../../logParsing/plot_new.sh boolector.csv ablector.csv real | gnuplot
+mkdir -p results/basic
+cd results/basic
+python ../../../logParsing/buildCsv.py 2 ../../../Logs/Ablector_Mul/segment_aa/2019-06-17T16:50+02:00 | sort > ablector.csv
+python ../../../logParsing/buildCsv.py 2 ../../../Logs/Boolector_SoA/segment_aa/2019-06-17T16:49+02:00 | sort > boolector.csv
+../../../logParsing/plot_new.sh boolector.csv ablector.csv real | gnuplot
 mv boolector-vs-ablector.png boolector-vs-ablector-real.png
-../../logParsing/plot_new.sh boolector.csv ablector.csv time | gnuplot
+../../../logParsing/plot_new.sh boolector.csv ablector.csv time | gnuplot
 mv boolector-vs-ablector.png boolector-vs-ablector-time.png
-../../logParsing/plot_new.sh boolector.csv ablector.csv satpart | gnuplot
+../../../logParsing/plot_new.sh boolector.csv ablector.csv satpart | gnuplot
 mv boolector-vs-ablector.png boolector-vs-ablector-satpart.png
-python ../../logParsing/histData.py 2 ../../Logs/Ablector_Mul/segment_aa/2019-06-17T16:50+02:00 level.csv bit.csv
-../../logParsing/plot_hist.sh 3 level.csv | gnuplot
-../../logParsing/plot_hist.sh 3 bit.csv | gnuplot
+python ../../../logParsing/histData.py 2 ../../../Logs/Ablector_Mul/segment_aa/2019-06-17T16:50+02:00 level.csv bit.csv
+../../../logParsing/plot_hist.sh 3 level.csv | gnuplot
+../../../logParsing/plot_hist.sh 3 bit.csv | gnuplot
+rm -f *.tmp
+
+cd ../../
+
+mkdir -p results/singleStage
+cd results/singleStage
+python ../../../logParsing/buildCsv.py 2 ../../../Logs/Ablector_Mul/segment_aa/2019-06-19T22:40+02:00 | sort > ablector.csv
+python ../../../logParsing/buildCsv.py 1 ../../../Logs/Boolector_SoA/segment_aa/2019-06-19T22:40+02:00 | sort > boolector.csv
+../../../logParsing/plot_new.sh boolector.csv ablector.csv real | gnuplot
+mv boolector-vs-ablector.png boolector-vs-ablector-real.png
+../../../logParsing/plot_new.sh boolector.csv ablector.csv time | gnuplot
+mv boolector-vs-ablector.png boolector-vs-ablector-time.png
+../../../logParsing/plot_new.sh boolector.csv ablector.csv satpart | gnuplot
+mv boolector-vs-ablector.png boolector-vs-ablector-satpart.png
+python ../../../logParsing/histData.py 2 ../../../Logs/Ablector_Mul/segment_aa/2019-06-17T16:50+02:00 level.csv bit.csv
+../../../logParsing/plot_hist.sh 3 level.csv | gnuplot
+../../../logParsing/plot_hist.sh 3 bit.csv | gnuplot
 rm -f *.tmp
