57268ca919a847341ae051d5dd040b7c3a1a048c
Samuel Teuber <samuel@samweb.org>
2019-07-06 13:07:17 +0200

    Refactoring of plot scripts

--- Ablector_Mul/compare.sh
+++ Ablector_Mul/compare.sh
@@ -1,174 +1,56 @@
-mkdir -p results/basic
-cd results/basic
-python ../../../logParsing/buildCsv.py 2 ../../../Logs/Ablector_Mul/segment_aa/2019-06-17T16:50+02:00 | sort > ablector.csv
-python ../../../logParsing/buildCsv.py 1 ../../../Logs/Boolector_SoA/segment_aa/2019-06-19T22:40+02:00 | sort > boolector.csv
-../../../logParsing/plot_new.sh boolector.csv ablector.csv real | gnuplot
-mv boolector-vs-ablector.png boolector-vs-ablector-real.png
-mv boolector-vs-ablector.csv boolector-vs-ablector-real.csv
-../../../logParsing/plot_new.sh boolector.csv ablector.csv time | gnuplot
-mv boolector-vs-ablector.png boolector-vs-ablector-time.png
-mv boolector-vs-ablector.csv boolector-vs-ablector-time.csv
-../../../logParsing/plot_new.sh boolector.csv ablector.csv satpart | gnuplot
-mv boolector-vs-ablector.png boolector-vs-ablector-satpart.png
-mv boolector-vs-ablector.csv boolector-vs-ablector-satpart.csv
-python ../../../logParsing/histData.py 2 ../../../Logs/Ablector_Mul/segment_aa/2019-06-17T16:50+02:00 MulNode level.csv bit.csv
-../../../logParsing/plot_hist.sh 3 level.csv | gnuplot
-../../../logParsing/plot_hist.sh 3 bit.csv | gnuplot
-rm -f *.tmp
+#! /bin/bash
 
-cd ../../
+source ../logParsing/functions.sh
 
-mkdir -p results/singleStage
-cd results/singleStage
-python ../../../logParsing/buildCsv.py 2 ../../../Logs/Ablector_Mul/segment_aa/2019-06-19T22:40+02:00 | sort > ablector.csv
-python ../../../logParsing/buildCsv.py 1 ../../../Logs/Boolector_SoA/segment_aa/2019-06-19T22:40+02:00 | sort > boolector.csv
-../../../logParsing/plot_new.sh boolector.csv ablector.csv real | gnuplot
-mv boolector-vs-ablector.png boolector-vs-ablector-real.png
-mv boolector-vs-ablector.csv boolector-vs-ablector-real.csv
-../../../logParsing/plot_new.sh boolector.csv ablector.csv time | gnuplot
-mv boolector-vs-ablector.png boolector-vs-ablector-time.png
-mv boolector-vs-ablector.csv boolector-vs-ablector-time.csv
-../../../logParsing/plot_new.sh boolector.csv ablector.csv satpart | gnuplot
-mv boolector-vs-ablector.png boolector-vs-ablector-satpart.png
-mv boolector-vs-ablector.csv boolector-vs-ablector-satpart.csv
-python ../../../logParsing/histData.py 2 ../../../Logs/Ablector_Mul/segment_aa/2019-06-19T22:40+02:00 MulNode level.csv bit.csv
-../../../logParsing/plot_hist.sh 3 level.csv | gnuplot
-../../../logParsing/plot_hist.sh 3 bit.csv | gnuplot
-rm -f *.tmp
+if newDir results/basic
+then
+    compare ablector Ablector_Mul/segment_aa/2019-06-17T16:50+02:00 2 boolector Boolector_SoA/segment_aa/2019-06-19T22:40+02:00 1 real time satpart
+    buildHist Ablector_Mul/segment_aa/2019-06-17T16:50+02:00 MulNode 2
+fi
 
-cd ../../
+if newDir results/singleStage
+then
+    compare ablector Ablector_Mul/segment_aa/2019-06-19T22:40+02:00 2 boolector Boolector_SoA/segment_aa/2019-06-19T22:40+02:00 1 real time satpart
+    buildHist Ablector_Mul/segment_aa/2019-06-19T22:40+02:00 MulNode 2
+fi
 
-mkdir -p results/belatedUF
-cd results/belatedUF
-python ../../../logParsing/buildCsv.py 2 ../../../Logs/Ablector_Mul/segment_aa/2019-06-24T15:08+02:00 | sort > ablector.csv
-python ../../../logParsing/buildCsv.py 1 ../../../Logs/Boolector_SoA/segment_aa/2019-06-19T22:40+02:00 | sort > boolector.csv
-../../../logParsing/plot_new.sh boolector.csv ablector.csv real | gnuplot
-mv boolector-vs-ablector.png boolector-vs-ablector-real.png
-mv boolector-vs-ablector.csv boolector-vs-ablector-real.csv
-../../../logParsing/plot_new.sh boolector.csv ablector.csv time | gnuplot
-mv boolector-vs-ablector.png boolector-vs-ablector-time.png
-mv boolector-vs-ablector.csv boolector-vs-ablector-time.csv
-../../../logParsing/plot_new.sh boolector.csv ablector.csv satpart | gnuplot
-mv boolector-vs-ablector.png boolector-vs-ablector-satpart.png
-mv boolector-vs-ablector.csv boolector-vs-ablector-satpart.csv
-python ../../../logParsing/histData.py 2 ../../../Logs/Ablector_Mul/segment_aa/2019-06-24T15:08+02:00 MulNode level.csv bit.csv
-../../../logParsing/plot_hist.sh 3 level.csv | gnuplot
-../../../logParsing/plot_hist.sh 3 bit.csv | gnuplot
-rm -f *.tmp
+if newDir results/belatedUF
+then
+    compare ablector Ablector_Mul/segment_aa/2019-06-24T15:08+02:00 2 boolector Boolector_SoA/segment_aa/2019-06-19T22:40+02:00 1 real time satpart
+    buildHist Ablector_Mul/segment_aa/2019-06-24T15:08+02:00 MulNode 2
+fi
 
-cd ../../
+if newDir results/belatedUF-all
+then
+    compare ablector Ablector_Mul/all/2019-06-26T15:59+02:00 2 boolector Boolector_SoA/all/2019-06-26T15:59+02:00 1 real time satpart
+    buildHist Ablector_Mul/all/2019-06-26T15:59+02:00 MulNode 2
+fi
 
-mkdir -p results/belatedUF-all
-cd results/belatedUF-all
-python ../../../logParsing/buildCsv.py 2 ../../../Logs/Ablector_Mul/all/2019-06-26T15:59+02:00 | sort > ablector.csv
-python ../../../logParsing/buildCsv.py 1 ../../../Logs/Boolector_SoA/all/2019-06-26T15:59+02:00 | sort > boolector.csv
-../../../logParsing/plot_new.sh boolector.csv ablector.csv real | gnuplot
-mv boolector-vs-ablector.png boolector-vs-ablector-real.png
-mv boolector-vs-ablector.csv boolector-vs-ablector-real.csv
-../../../logParsing/plot_new.sh boolector.csv ablector.csv time | gnuplot
-mv boolector-vs-ablector.png boolector-vs-ablector-time.png
-mv boolector-vs-ablector.csv boolector-vs-ablector-time.csv
-../../../logParsing/plot_new.sh boolector.csv ablector.csv satpart | gnuplot
-mv boolector-vs-ablector.png boolector-vs-ablector-satpart.png
-mv boolector-vs-ablector.csv boolector-vs-ablector-satpart.csv
-python ../../../logParsing/histData.py 2 ../../../Logs/Ablector_Mul/all/2019-06-26T15:59+02:00 MulNode level.csv bit.csv
-../../../logParsing/plot_hist.sh 3 level.csv | gnuplot
-../../../logParsing/plot_hist.sh 3 bit.csv | gnuplot
-rm -f *.tmp
+if newDir results/belatedUF-rewrite
+then
+    compare ablector Ablector_Mul/segment_aa/2019-06-30T09:57+02:00 2 boolector Boolector_SoA/segment_aa/2019-06-19T22:40+02:00 1 real time satpart
+    buildHist Ablector_Mul/segment_aa/2019-06-30T09:57+02:00 MulNode 2
+fi
 
-cd ../../
+if newDir results/interval-mul
+then
+    compare ablector Ablector_Mul/all/2019-07-05T11:45+02:00 2 boolector Boolector_SoA/all/2019-06-26T15:59+02:00 1 real time satpart
+    buildHist Ablector_Mul/all/2019-07-05T11:45+02:00 MulNode 2
+fi
 
-mkdir -p results/belatedUF-rewrite
-cd results/belatedUF-rewrite
-python ../../../logParsing/buildCsv.py 2 ../../../Logs/Ablector_Mul/segment_aa/2019-06-30T09:57+02:00 | sort > ablector.csv
-python ../../../logParsing/buildCsv.py 1 ../../../Logs/Boolector_SoA/segment_aa/2019-06-19T22:40+02:00 | sort > boolector.csv
-../../../logParsing/plot_new.sh boolector.csv ablector.csv real | gnuplot
-mv boolector-vs-ablector.png boolector-vs-ablector-real.png
-mv boolector-vs-ablector.csv boolector-vs-ablector-real.csv
-../../../logParsing/plot_new.sh boolector.csv ablector.csv time | gnuplot
-mv boolector-vs-ablector.png boolector-vs-ablector-time.png
-mv boolector-vs-ablector.csv boolector-vs-ablector-time.csv
-../../../logParsing/plot_new.sh boolector.csv ablector.csv satpart | gnuplot
-mv boolector-vs-ablector.png boolector-vs-ablector-satpart.png
-mv boolector-vs-ablector.csv boolector-vs-ablector-satpart.csv
-python ../../../logParsing/histData.py 2 ../../../Logs/Ablector_Mul/segment_aa/2019-06-30T09:57+02:00 MulNode level.csv bit.csv
-../../../logParsing/plot_hist.sh 3 level.csv | gnuplot
-../../../logParsing/plot_hist.sh 3 bit.csv | gnuplot
-rm -f *.tmp
+if newDir results/interval-mul-vs-belated-uf
+then
+    compare interval Ablector_Mul/all/2019-07-05T11:45+02:00 2 bitwise Ablector_Mul/all/2019-06-26T15:59+02:00 2 real time satpart
+fi
 
-cd ../../
+if newDir results/sdiv-mul-interval
+then
+    compare ablector Ablector_Mul/all/2019-07-05T16:17+02:00 2 boolector Boolector_SoA/all/2019-06-26T15:59+02:00 1 real time satpart
+    buildHist Ablector_Mul/all/2019-07-05T16:17+02:00 MulNode 2
+    buildHist Ablector_Mul/all/2019-07-05T16:17+02:00 SdivNode 2
+fi
 
-mkdir -p results/interval-mul
-cd results/interval-mul
-python ../../../logParsing/buildCsv.py 2 ../../../Logs/Ablector_Mul/all/2019-07-05T11:45+02:00 | sort > ablector.csv
-python ../../../logParsing/buildCsv.py 1 ../../../Logs/Boolector_SoA/all/2019-06-26T15:59+02:00 | sort > boolector.csv
-../../../logParsing/plot_new.sh boolector.csv ablector.csv real | gnuplot
-mv boolector-vs-ablector.png boolector-vs-ablector-real.png
-mv boolector-vs-ablector.csv boolector-vs-ablector-real.csv
-../../../logParsing/plot_new.sh boolector.csv ablector.csv time | gnuplot
-mv boolector-vs-ablector.png boolector-vs-ablector-time.png
-mv boolector-vs-ablector.csv boolector-vs-ablector-time.csv
-../../../logParsing/plot_new.sh boolector.csv ablector.csv satpart | gnuplot
-mv boolector-vs-ablector.png boolector-vs-ablector-satpart.png
-mv boolector-vs-ablector.csv boolector-vs-ablector-satpart.csv
-python ../../../logParsing/histData.py 2 ../../../Logs/Ablector_Mul/all/2019-07-05T11:45+02:00 MulNode level.csv bit.csv
-../../../logParsing/plot_hist.sh 3 level.csv | gnuplot
-../../../logParsing/plot_hist.sh 3 bit.csv | gnuplot
-rm -f *.tmp
-
-cd ../../
-
-mkdir -p results/interval-mul-vs-belated-uf
-cd results/interval-mul-vs-belated-uf
-python ../../../logParsing/buildCsv.py 2 ../../../Logs/Ablector_Mul/all/2019-07-05T11:45+02:00 | sort > interval.csv
-python ../../../logParsing/buildCsv.py 2 ../../../Logs/Ablector_Mul/all/2019-06-26T15:59+02:00 | sort > bitwise.csv
-../../../logParsing/plot_new.sh bitwise.csv interval.csv real | gnuplot
-mv bitwise-vs-interval.png bitwise-vs-interval-real.png
-mv bitwise-vs-interval.csv bitwise-vs-interval-real.csv
-../../../logParsing/plot_new.sh bitwise.csv interval.csv time | gnuplot
-mv bitwise-vs-interval.png bitwise-vs-interval-time.png
-mv bitwise-vs-interval.csv bitwise-vs-interval-time.csv
-../../../logParsing/plot_new.sh bitwise.csv interval.csv satpart | gnuplot
-mv bitwise-vs-interval.png bitwise-vs-interval-satpart.png
-mv bitwise-vs-interval.csv bitwise-vs-interval-satpart.csv
-rm -f *.tmp
-
-cd ../../
-
-mkdir -p results/sdiv-mul-interval
-cd results/sdiv-mul-interval
-python ../../../logParsing/buildCsv.py 2 ../../../Logs/Ablector_Mul/all/2019-07-05T16:17+02:00 | sort > ablector.csv
-python ../../../logParsing/buildCsv.py 1 ../../../Logs/Boolector_SoA/all/2019-06-26T15:59+02:00 | sort > boolector.csv
-../../../logParsing/plot_new.sh boolector.csv ablector.csv real | gnuplot
-mv boolector-vs-ablector.png boolector-vs-ablector-real.png
-mv boolector-vs-ablector.csv boolector-vs-ablector-real.csv
-../../../logParsing/plot_new.sh boolector.csv ablector.csv time | gnuplot
-mv boolector-vs-ablector.png boolector-vs-ablector-time.png
-mv boolector-vs-ablector.csv boolector-vs-ablector-time.csv
-../../../logParsing/plot_new.sh boolector.csv ablector.csv satpart | gnuplot
-mv boolector-vs-ablector.png boolector-vs-ablector-satpart.png
-mv boolector-vs-ablector.csv boolector-vs-ablector-satpart.csv
-python ../../../logParsing/histData.py 2 ../../../Logs/Ablector_Mul/all/2019-07-05T16:17+02:00 MulNode level-mul.csv bit-mul.csv
-python ../../../logParsing/histData.py 2 ../../../Logs/Ablector_Mul/all/2019-07-05T16:17+02:00 SdivNode level-sdiv.csv bit-sdiv.csv
-../../../logParsing/plot_hist.sh 3 level-mul.csv | gnuplot
-../../../logParsing/plot_hist.sh 3 bit-mul.csv | gnuplot
-../../../logParsing/plot_hist.sh 3 level-sdiv.csv | gnuplot
-../../../logParsing/plot_hist.sh 3 bit-sdiv.csv | gnuplot
-rm -f *.tmp
-
-cd ../../
-
-mkdir -p results/mul-vs-sdiv-mul
-cd results/mul-vs-sdiv-mul
-python ../../../logParsing/buildCsv.py 2 ../../../Logs/Ablector_Mul/all/2019-07-05T16:17+02:00 | sort > sdiv-mul.csv
-python ../../../logParsing/buildCsv.py 2 ../../../Logs/Ablector_Mul/all/2019-07-05T11:45+02:00 | sort > mul.csv
-../../../logParsing/plot_new.sh mul.csv sdiv-mul.csv real | gnuplot
-mv mul-vs-sdiv-mul.png mul-vs-sdiv-mul-real.png
-mv mul-vs-sdiv-mul.csv mul-vs-sdiv-mul-real.csv
-../../../logParsing/plot_new.sh mul.csv sdiv-mul.csv time | gnuplot
-mv mul-vs-sdiv-mul.png mul-vs-sdiv-mul-time.png
-mv mul-vs-sdiv-mul.csv mul-vs-sdiv-mul-time.csv
-../../../logParsing/plot_new.sh mul.csv sdiv-mul.csv satpart | gnuplot
-mv mul-vs-sdiv-mul.png mul-vs-sdiv-mul-satpart.png
-mv mul-vs-sdiv-mul.csv mul-vs-sdiv-mul-satpart.csv
-rm -f *.tmp
+if newDir results/mul-vs-sdiv-mul
+then
+    compare sdiv-mul Ablector_Mul/all/2019-07-05T16:17+02:00 2 mul Ablector_Mul/all/2019-07-05T11:45+02:00 2 real time satpart
+fi
---
+++ logParsing/functions.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+CALL_CWD=`pwd`
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+function newDir {
+    cd $CALL_CWD
+    if [ -d "$1" ]
+    then
+        return 1 # false
+    else
+        mkdir -p $1
+        cd $1
+        return 0 # true
+    fi
+}
+
+function compare {
+    F1=$1
+    F2=$4
+    python $DIR/buildCsv.py $3 $DIR/../Logs/$2 | sort > $1.csv
+    python $DIR/buildCsv.py $6 $DIR/../Logs/$5 | sort > $4.csv
+    shift 6
+    while (( "$#" )); do
+        $DIR/plot_new.sh $F2.csv $F1.csv $1 | gnuplot
+        mv $F2-vs-$F1.png $F2-vs-$F1-$1.png
+        mv $F2-vs-$F1.csv $F2-vs-$F1-$1.csv
+        shift
+    done
+    return 0
+}
+
+function buildHist {
+    python $DIR/histData.py $3 $DIR/../Logs/$1 $2 level-$2.csv bit-$2.csv
+    $DIR/plot_hist.sh 3 level-$2.csv | gnuplot
+    $DIR/plot_hist.sh 3 bit-$2.csv | gnuplot
+}
