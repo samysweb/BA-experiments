825ec738236dd0d4267d957318eb3eec57bafcfc
Samuel Teuber <samuel@samweb.org>
2019-07-07 16:34:07 +0200

    Updated plots

--- Ablector_Mul/compare.sh
+++ Ablector_Mul/compare.sh
@@ -54,3 +54,14 @@ if newDir results/mul-vs-sdiv-mul
 then
     compare sdiv-mul Ablector_Mul/all/2019-07-06T12:27+02:00 2 mul Ablector_Mul/all/2019-07-05T11:45+02:00 2 real time satpart
 fi
+
+if newDir results/belatedUF-all-fullMul
+then
+    compare ablector Ablector_Mul/all/2019-07-06T16:11+02:00 2 boolector Boolector_SoA/all/2019-06-26T15:59+02:00 1 real time satpart
+    buildHist Ablector_Mul/all/2019-07-06T16:11+02:00 MulNode 2 2
+fi
+
+if newDir results/belatedUF-all-fullMul-vs-self
+then
+    compare mul Ablector_Mul/all/2019-07-06T16:11+02:00 2 selfmade Ablector_Mul/all/2019-06-26T15:59+02:00 2 real time satpart
+fi
