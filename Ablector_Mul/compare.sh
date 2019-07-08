52a3efb0b5b823386c97f86a462f55b4b3beda21
Samuel Teuber <samuel@samweb.org>
2019-07-08 15:26:34 +0200

    Refactored directory structure for plotting

--- Ablector_Mul/compare.sh
+++ Ablector_Mul/compare.sh
@@ -2,66 +2,66 @@
 
 source ../logParsing/functions.sh
 
-if newDir results/basic
+if newDir results/segment_aa/basic
 then
     compare ablector Ablector_Mul/segment_aa/2019-06-17T16:50+02:00 2 boolector Boolector_SoA/segment_aa/2019-06-19T22:40+02:00 1 real time satpart
     buildHist Ablector_Mul/segment_aa/2019-06-17T16:50+02:00 MulNode 2 2
 fi
 
-if newDir results/singleStage
+if newDir results/segment_aa/singleStage
 then
     compare ablector Ablector_Mul/segment_aa/2019-06-19T22:40+02:00 2 boolector Boolector_SoA/segment_aa/2019-06-19T22:40+02:00 1 real time satpart
     buildHist Ablector_Mul/segment_aa/2019-06-19T22:40+02:00 MulNode 2 2
 fi
 
-if newDir results/belatedUF
+if newDir results/segment_aa/belatedUF
 then
     compare ablector Ablector_Mul/segment_aa/2019-06-24T15:08+02:00 2 boolector Boolector_SoA/segment_aa/2019-06-19T22:40+02:00 1 real time satpart
     buildHist Ablector_Mul/segment_aa/2019-06-24T15:08+02:00 MulNode 2 2
 fi
 
-if newDir results/belatedUF-all
+if newDir results/all/belatedUF
 then
     compare ablector Ablector_Mul/all/2019-06-26T15:59+02:00 2 boolector Boolector_SoA/all/2019-06-26T15:59+02:00 1 real time satpart
     buildHist Ablector_Mul/all/2019-06-26T15:59+02:00 MulNode 2 2
 fi
 
-if newDir results/belatedUF-rewrite
+if newDir results/segment_aa/belatedUF-rewrite
 then
     compare ablector Ablector_Mul/segment_aa/2019-06-30T09:57+02:00 2 boolector Boolector_SoA/segment_aa/2019-06-19T22:40+02:00 1 real time satpart
     buildHist Ablector_Mul/segment_aa/2019-06-30T09:57+02:00 MulNode 2 2
 fi
 
-if newDir results/interval-mul
+if newDir results/all/interval-mul
 then
     compare ablector Ablector_Mul/all/2019-07-05T11:45+02:00 2 boolector Boolector_SoA/all/2019-06-26T15:59+02:00 1 real time satpart
     buildHist Ablector_Mul/all/2019-07-05T11:45+02:00 MulNode 2 2
 fi
 
-if newDir results/interval-mul-vs-belated-uf
+if newDir results/all/interval-mul-vs-belated-uf
 then
     compare interval Ablector_Mul/all/2019-07-05T11:45+02:00 2 bitwise Ablector_Mul/all/2019-06-26T15:59+02:00 2 real time satpart
 fi
 
-if newDir results/sdiv-mul-interval
+if newDir results/all/sdiv-mul-interval
 then
     compare ablector Ablector_Mul/all/2019-07-06T12:27+02:00 2 boolector Boolector_SoA/all/2019-06-26T15:59+02:00 1 real time satpart
     buildHist Ablector_Mul/all/2019-07-06T12:27+02:00 MulNode 2 3
     buildHist Ablector_Mul/all/2019-07-06T12:27+02:00 SdivNode 2 3
 fi
 
-if newDir results/mul-vs-sdiv-mul
+if newDir results/all/mul-vs-sdiv-mul
 then
     compare sdiv-mul Ablector_Mul/all/2019-07-06T12:27+02:00 2 mul Ablector_Mul/all/2019-07-05T11:45+02:00 2 real time satpart
 fi
 
-if newDir results/belatedUF-all-fullMul
+if newDir results/all/belatedUF-fullMul
 then
     compare ablector Ablector_Mul/all/2019-07-06T16:11+02:00 2 boolector Boolector_SoA/all/2019-06-26T15:59+02:00 1 real time satpart
     buildHist Ablector_Mul/all/2019-07-06T16:11+02:00 MulNode 2 2
 fi
 
-if newDir results/belatedUF-all-fullMul-vs-self
+if newDir results/all/belatedUF-fullMul-vs-self
 then
     compare mul Ablector_Mul/all/2019-07-06T16:11+02:00 2 selfmade Ablector_Mul/all/2019-06-26T15:59+02:00 2 real time satpart
 fi
