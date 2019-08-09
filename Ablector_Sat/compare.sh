dc9b6d471e44e049114d0a5a0b8f1b868e48e8e9
Samuel Teuber <samuel@samweb.org>
2019-08-09 11:27:07 +0200

    Updated plot script to use nonparallel boolector run

--- Ablector_Sat/compare.sh
+++ Ablector_Sat/compare.sh
@@ -5,7 +5,7 @@ source ../logParsing/functions.sh
 
 if newDir results/sat/sdiv-pow2-rewriting-repaired
 then
-    compare sdiv Ablector_Mul/sat/2019-07-22T10:04+02:00 2 boolector Boolector_SoA/sat/2019-07-23T17:04+02:00 1 real time satpart
+    compare sdiv Ablector_Mul/sat/2019-07-22T10:04+02:00 2 boolector Boolector_SoA/sat/2019-08-08T10:50+02:00 1 real time satpart
     buildHist Ablector_Mul/sat/2019-07-22T10:04+02:00 MulNode 2 3
     buildHist Ablector_Mul/sat/2019-07-22T10:04+02:00 SdivNode 2 3
 fi
