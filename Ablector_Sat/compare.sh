4492bb85aee3ff898ceaf148e0f34b10a7a79257
Samuel Teuber <samuel@samweb.org>
2019-08-17 09:28:29 +0200

    Various logs and evaluation of ufReuse1

--- Ablector_Sat/compare.sh
+++ Ablector_Sat/compare.sh
@@ -9,3 +9,10 @@ then
     buildHist Ablector_Mul/sat/2019-07-22T10:04+02:00 MulNode 2 3
     buildHist Ablector_Mul/sat/2019-07-22T10:04+02:00 SdivNode 2 3
 fi
+
+if newDir results/sat/sdiv-ufReuse1
+then
+    compare ablector Ablector_Mul/sat/2019-08-10T18:49+02:00 2 boolector Boolector_SoA/sat/2019-08-08T10:50+02:00 1 real time satpart
+    buildHist Ablector_Mul/sat/2019-08-10T18:49+02:00 MulNode 2 3
+    buildHist Ablector_Mul/sat/2019-08-10T18:49+02:00 SdivNode 2 3
+fi
--- Logs/Ablector_Mul/sat/README.md
+++ Logs/Ablector_Mul/sat/README.md
@@ -1,3 +1,9 @@
 # 2019-07-22T10:04+02:00
 Ablector commit: `3b25ada04f3434edcaae6fd5d8aa829e4957062a`
 SAT results for fixed (!) version of mul, sdiv, srem
+# 2019-08-09T12:02+02:00
+Ablector commit: `4301716753e46e50a413c6b676f1d98d3a50f37d`
+Bug!
+# 2019-08-10T18:49+02:00
+Ablector commit: `c7acfbb958dbb9455de80a554f6ebcebb9f2423a`
+ufReuse=1
